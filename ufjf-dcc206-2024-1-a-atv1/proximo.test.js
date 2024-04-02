1590606f59580c2d8f65301ec05b27e06992f062
isaac kapela <124640429+isaac-kapela@users.noreply.github.com>
2024-04-02 20:43:00 -0300

    atividade 1

---
+++ ufjf-dcc206-2024-1-a-atv1/proximo.test.js
@@ -0,0 +1,18 @@
+import {test, expect} from 'vitest';
+import {proximo} from "./proximo"
+
+test('proximo(0) retorna 1', () =>{
+    expect(proximo(0)).toBe(1);
+});
+
+test('proximo(1) retorna 3', () =>{
+    expect(proximo(1)).toBe(3);
+});
+
+test('proximo(2) retorna 5', () =>{
+    expect(proximo(2)).toBe(5);
+});
+
+test('proximo(3) retorna 4', () =>{
+    expect(proximo(3)).toBe(4);
+});
